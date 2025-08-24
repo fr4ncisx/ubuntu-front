2209ad939acbbd54b60eebb9d91c0e3330efd8d9
fr4ncisx <fr4ncisx@gmail.com>
2025-08-24 04:00:57 -0300

    fix: changed baseURL to local

--- src/scripts/axiosConfig.ts
+++ src/scripts/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const apiClient: AxiosInstance = axios.create({
-  baseURL: "https://ubuntu.koyeb.app",
+  baseURL: "http://localhost:8080",
   timeout: 50000,
   headers: {
     "Content-Type": "application/json",
